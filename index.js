8261c66b1de8ac1216df498d94b56a2022d77129
tranjg <tranjg@mail.uc.edu>
2020-10-08 19:41:01 -0400

    created displayWeather function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const notificationElement = document.querySelector(`.notification`);
-const iconElement = document.querySelector(`.weather-icon`);
-const tempElement = document.querySelector(`.temperature-value  p`);
-const descElement = document.querySelector(`.temperature-description p`);
-const locationElement = document.querySelector(`.location p`);
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value  p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
 
 const weather = {};
 
 weather.temperature = {
-    unit : `fahrenheit`
+    unit : "celsius"
 }
 
 const KELVIN = 273;
@@ -20,11 +20,11 @@ if(`geolocation` in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
     notificationElement.style.display=`block`;
-    notificationElement.innerHTML = `<p>Brwoser doesn't support Geolocation</p>`;
+    notificationElement.innerHTML = `<p>Browser doesn't support Geolocation</p>`;
 }
 
 //set user's position
-function setPostion(position) {
+function setPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
@@ -47,8 +47,9 @@ function getWeather(latitude, longitude) {
             return data;
         })
         .then(function(data) {
-            weather.temperature.value = Math.floor(data.main.temp = KELVIN);
-            weather.description = data.weather[0].icon;
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconID = data.weather[0].icon;
             weather.city = data.name;
             weather.country = data.sys.country;
         })
@@ -57,4 +58,11 @@ function getWeather(latitude, longitude) {
         });
 }
 
-//display weather
+//display weather to ui
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
