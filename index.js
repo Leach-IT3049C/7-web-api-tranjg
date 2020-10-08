721134e5d8b14e8cef885269d9a9e299fc9c2541
tranjg <tranjg@mail.uc.edu>
2020-10-08 17:52:26 -0400

    created browser suppport check; set user's position

--- index.js
+++ index.js
@@ -6,4 +6,28 @@ const locationElement = document.querySelector(`.location p`);
 
 const weather = {};
 
+weather.temperature = {
+    unit : `fahrenheit`
+}
+
+const KELVIN = 273;
+
+//api key
+const key = `e3eeba4b19afc269da61cbeca2c04699`;
+
+//check for browser support of geolocation
+if(`geolocation` in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display=`block`;
+    notificationElement.innerHTML = `<p>Brwoser doesn't support Geolocation</p>`;
+}
+
+//set user's position
+function setPostion(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
 
