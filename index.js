40ba5ed4c7cf7c5e8412b29e170d2d620fc6629d
tranjg <tranjg@mail.uc.edu>
2020-10-08 19:57:33 -0400

    fixed icon problem

--- index.js
+++ index.js
@@ -33,20 +33,20 @@ function setPosition(position) {
 
 //show error when there's an issue
 function showError(error){
-    notificationElement.style.display = `block`;
+    notificationElement.style.display = "block";
     notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
 
 //get weather from api
-function getWeather(latitude, longitude) {
+function getWeather(latitude, longitude){
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
-        .then(function(response) {
+        .then(function(response){
             let data = response.json();
             return data;
         })
-        .then(function(data) {
+        .then(function(data){
             weather.temperature.value = Math.floor(data.main.temp - KELVIN);
             weather.description = data.weather[0].description;
             weather.iconID = data.weather[0].icon;
@@ -65,4 +65,27 @@ function displayWeather(){
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
+
+//c to f conversion
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 9/5) + 32;
+}
+
+// when the user clicks on temp
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+
+    if(weather.temperature.unit == "celsius") {
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
+
+
 
